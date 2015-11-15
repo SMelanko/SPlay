688f5f6d35095c8b386178cbf699d96a1b252344
Slava <vandernyt23@gmail.com>
2015-11-15 15:29:52 +0200

    1. Copied cppformat and spdlog in the external dir. 2. Added sereal serialization library.

--- src/cppformat/format.cc
+++ src/cppformat/format.cc
@@ -25,7 +25,7 @@
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "cppformat/format.h"
+#include "cppformat\format.h"
 
 #include <string.h>
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,66 @@
 #include <iostream>
-#include "cppformat/format.h"
-#include "spdlog/spdlog.h"
+#include <cereal\archives\binary.hpp>
+#include <cereal\archives\json.hpp>
+#include <cereal\types\memory.hpp>
+#include <cereal\types\unordered_map.hpp>
+#include <fstream>
+#include "cppformat\format.h"
+#include "spdlog\spdlog.h"
+# if 0
+struct Record
+{
+	uint8_t x, y;
+	double z;
+
+	template<class Archive>
+	void serialize(Archive& ar)
+	{
+		ar(x, y, z);
+	}
+};
+#endif
+struct Data
+{
+	int32_t id;
+	std::string name;
+	std::string surname;
+
+	template <class Archive>
+	void serialize(Archive& ar)
+	{
+		ar(CEREAL_NVP(name), CEREAL_NVP(surname));
+	}
+#if 0
+	template <class Archive>
+	void load(Archive& ar)
+	{
+		static int32_t idGen = 0;
+		id = ++idGen;
+		ar(name, surname);
+	}
+#endif
+};
 
 int main()
 {
 	std::string str{ fmt::format("Hello {}!\n", "Slava") };
 	fmt::print(str);
 
-	//Create console, multithreaded logger
-	auto console = spdlog::stdout_logger_mt("console");
-
-	//
-	// Create a daily logger - a new file is created every day on 2:30am
-	//
-	auto daily_logger = spdlog::daily_logger_mt("daily_logger", "daily", 23, 51);
-
-	//
 	// Create a file rotating logger with 5mb size max and 3 rotated files
-	//
 	auto log = spdlog::rotating_logger_mt("file_logger", "mylogfile", 10240, 3);
 	for (int i = 200; i < 300; ++i)
-		daily_logger->info("{} * {} equals {:>10}", i, i, i*i);
+		log->info("{} * {} equals {:>10}", i, i, i*i);
 
-	spdlog::drop_all();
+	std::ofstream os{ "test.json", std::ios::binary };
+	cereal::JSONOutputArchive archive{ os };
+
+	Data data;
+	data.name = std::move(std::string{ "Slava" });
+	data.surname = std::string{ "Melanko" };
+	//data.data = std::make_shared<std::unordered_map<uint32_t, Record>>();
+	//data.data->insert(std::make_pair<uint32_t, Record>(
+	//	123, Record{ 1,2,3.3 }));
+	archive(cereal::make_nvp("User", data));
 
 	return 0;
 }
